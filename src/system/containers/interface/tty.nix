714a09e6c01bf9f9278481a49545aaccb42258c5
Dani <dani@hyders.dev>
2025-12-30 04:56:46 -0500

    only set first tty to be autologin

--- src/system/containers/interface/tty.nix
+++ src/system/containers/interface/tty.nix
@@ -12,5 +12,11 @@
     mode = "0444";
   };
 
-  services.getty.autologinUser = "hydride-containers";
+  systemd.services."getty@tty1" = {
+    overrideStrategy = "asDropin";
+    serviceConfig.ExecStart = [
+      ""
+      "@${pkgs.util-linux}/sbin/agetty agetty --login-program ${config.services.getty.loginProgram} --autologin hydride-containers --noclear --keep-baud %I 115200,38400,9600 $TERM"
+    ];
+  };
 }
